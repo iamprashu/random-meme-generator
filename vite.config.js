231e2cf973bbbc1d5f710aa11886ce3c713dd01a
iamprashu <iamprashu.gs@gmail.com>
2025-04-11 12:15:04 +0530

    Deployment Setup Done All Data Has Been Saved Successfullt ---- node7

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(),tailwindcss()],server: {
     host: true
-  }
-
-
+  },
+  base:"/random-meme-generator",
 })
