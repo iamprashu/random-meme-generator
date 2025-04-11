54a689089f483f31644fe8e47482f5771b7b634d
iamprashu <iamprashu.gs@gmail.com>
2025-04-11 11:55:40 +0530

    src Components

--- src/components/RandomPanel.jsx
+++ src/components/RandomPanel.jsx
@@ -1,22 +1,20 @@
-import {useEffect, useState} from "react";
-
-import axios from "axios";
 import Loader from "./Loader.jsx";
 
 export default function RandomPanel(props){
-    const imgURL = props.imgURL;
     const isLoading = props.isLoading
+    const imgURL = props.imgURL
     const GetImage = props.GetImage;
-
+    const setQuery = props.setQuery;
+    console.log(imgURL);
 
     return(
         <div className="bg-black/1 backdrop-blur-xl min-h-[40%] w-[90%] md:w-[50%] rounded-2xl flex flex-col items-center p-5 gap-5">
             <div className='h-[500px]'>
-                {isLoading ?  (<Loader/>) : (<img src={imgURL} alt="" className="rounded-2xl h-[500px]"/>)}
+                {isLoading ?  (<Loader/>) : (<img src={imgURL} alt="" className="rounded-2xl h-[500px] max-w-[100%]" />)}
             </div>
             
             <div className="w-[100%] flex items-center justify-evenly">
-                <button  className="bg-gray-800 px-5 py-3 text-2xl text-white rounded-2xl" onClick={()=>{GetImage()}}>Generate Random Meme</button>
+                <button  className="bg-fuchsia-950 px-5 py-3 text-2xl text-white rounded-2xl hover:bg-fuchsia-900" onClick={()=>{setQuery('');  GetImage()}}>Generate Random Meme</button>
             </div>
 
         </div>
--- src/components/TagPanel.jsx
+++ src/components/TagPanel.jsx
@@ -2,23 +2,26 @@ import {useState} from "react";
 
 export default function TagPanel(props){
     const setQuery = props.setQuery;
-    const SearchImage = props.SearchImage;
-    const[topic,setTopic] = useState('')
+    const GetImage = props.GetImage;
 
-    function inputHandler(event){
-        setTopic(event.target.value)
-    }
+    const[searchData,setSearchData] = useState('');
+
     function submitData(event){
+        if(!searchData){
+            alert('Please Enter Query');
+        }
         event.preventDefault()
-        setQuery(topic)
-        setTopic('')
+        setQuery(searchData);
+        setSearchData('');
     }
+
     return(
         <div className="bg-black/1 backdrop-blur-xl min-h-[20%] w-[90%] md:w-[50%] rounded-2xl flex flex-col items-center p-5 gap-5">
             <h1 className="text-white text-xl font-bold">Enter topic here to get meme:</h1>
             <form className="flex flex-col justify-evenly h-[200px]" onSubmit={submitData}>
-                <input type="text" placeholder="Enter Topic..." className="h-15 w-100 border-2 rounded-4xl p-2 focus:outline-0 text-white text-xl" value={topic} onChange={inputHandler}/>
-                <button className="bg-gray-800 px-5 py-3 rounded-2xl text-white">Get Meme.</button>
+                <input type="text" placeholder="Enter Topic..." className="h-15 w-100 border-2 rounded-4xl p-2 focus:outline-0 text-white text-xl" value={searchData}
+                       onChange={(event)=>setSearchData(event.target.value)}/>;
+                <button className="bg-fuchsia-950 hover:bg-fuchsia-900 px-5 py-3 rounded-2xl text-white">Get Meme.</button>
             </form>
         </div>
     )
